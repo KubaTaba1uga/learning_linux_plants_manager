36cde9691b20f7f73c05c77f12cbfba859ff8a5f
KubaTaba1uga <luna7337anul@proton.me>
2025-03-20 09:31:30 +0100

    insmod modules on boot

--- buildroot/board/custom_rpi/post_build.sh
+++ buildroot/board/custom_rpi/post_build.sh
@@ -5,3 +5,13 @@ set -e
 
 find /lib/modules/*/updates -type f -name *driver*ko* -exec xz -d {} \;
 
+# If inittab exists, add a sysinit entry to load driver modules
+if [ -e "${TARGET_DIR}/etc/inittab" ]; then
+    # Add the modload entry if it doesn't already exist
+    if ! grep -q "::sysinit:/bin/sh -c 'find /lib/modules" "${TARGET_DIR}/etc/inittab"; then
+        echo "::sysinit:/bin/sh -c 'find /lib/modules/*/updates -type f -name \"*driver*ko*\" -exec insmod {} \;'" >> "${TARGET_DIR}/etc/inittab"
+    fi  
+else
+    echo "Error: ${TARGET_DIR}/etc/inittab not found." >&2
+    exit 2
+fi
