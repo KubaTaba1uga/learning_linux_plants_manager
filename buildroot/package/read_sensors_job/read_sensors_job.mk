53787250da80b4fb9abc46169b8eb800d40e8d8d
KubaTaba1uga <luna7337anul@proton.me>
2025-03-20 19:30:44 +0100

    fix buildroot bugs

--- buildroot/package/read_sensors_job/read_sensors_job.mk
+++ buildroot/package/read_sensors_job/read_sensors_job.mk
@@ -2,12 +2,11 @@ READ_SENSORS_JOB_VERSION = 1.0
 READ_SENSORS_JOB_SOURCE = read_sensors_job.py
 READ_SENSORS_JOB_SITE_METHOD = local
 READ_SENSORS_JOB_SITE = ../read_sensors_job
-READ_SENSORS_JOB_INSTALL_TARGET = usr/bin
 READ_SENSORS_JOB_DEPENDENCIES = python3
 
 define READ_SENSORS_JOB_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/read_sensors_job.py \
-		$(TARGET_DIR)/usr/bin/read_sensors_job.py
+	chmod 0755 $(@D)/read_sensors_job.py
+	cp $(@D)/read_sensors_job.py $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
--- buildroot/package/read_sensors_web/read_sensors_web.mk
+++ buildroot/package/read_sensors_web/read_sensors_web.mk
@@ -2,13 +2,13 @@ READ_SENSORS_WEB_VERSION = 1.0
 READ_SENSORS_WEB_SOURCE = read_sensors_web.py
 READ_SENSORS_WEB_SITE_METHOD = local
 READ_SENSORS_WEB_SITE = ../read_sensors_web
-READ_SENSORS_WEB_INSTALL_TARGET = usr/bin
 READ_SENSORS_WEB_DEPENDENCIES = python3
 READ_SENSORS_WEB_DEPENDENCIES += python-fastapi
 READ_SENSORS_WEB_DEPENDENCIES += python-uvicorn
 
 define READ_SENSORS_WEB_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0755 $(@D)/read_sensors_web.py $(TARGET_DIR)/$(READ_SENSORS_WEB_INSTALL_TARGET)/read_sensors_web.py
+	chmod 0755 $(@D)/read_sensors_web.py
+	cp $(@D)/read_sensors_web.py $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
