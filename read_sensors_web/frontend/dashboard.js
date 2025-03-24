735e94d1d76e892438c3ad24c70d161c9cd904ed
KubaTaba1uga <luna7337anul@proton.me>
2025-03-24 08:39:41 +0100

    cleanup bootstrap dashboard template

--- read_sensors_web/frontend/dashboard.js
+++ read_sensors_web/frontend/dashboard.js
@@ -8,7 +8,7 @@
   // Get the canvas context
   var air_temp_ctx = document.getElementById('air_temp');
   var air_humid_ctx = document.getElementById('air_humid');
-  var soil_0_humid_ctx = document.getElementById('soil_0_humid');
+  var soil_humid_ctx = document.getElementById('soil_humid');
     
   // Function to load sensor data and render chart
   async function loadSensorData() {
@@ -21,7 +21,6 @@
       const labels = dataRows.map(row => row.timestamp);
       const airTempData = dataRows.map(row => row.air_temp);
       const airHumidData = dataRows.map(row => row.air_humid);
-      const soil0HumidData = dataRows.map(row => row.soil_humid_0);
 	
       // Create a Chart.js line chart with the air temperature data
       new Chart(air_temp_ctx, {
@@ -102,21 +101,48 @@
             display: true
           }
         }
-      });	
-      // Create a Chart.js line chart with the air humidity data
-      new Chart(soil_0_humid_ctx, {
+      });
+
+
+	
+      // Create a Chart.js line chart with the soil humidity data
+      new Chart(soil_humid_ctx, {
         type: 'line',
         data: {
           labels: labels,
           datasets: [{
-            label: 'Soil 0 Humidity',
-            data: soil0HumidData,
+              label: 'Plant 0',
+            data: dataRows.map(row => row.soil_humid_0),
             lineTension: 0,
             backgroundColor: 'transparent',
-            borderColor: '#007bff',
+            borderColor: '#2aff00',
             borderWidth: 4,
-            pointBackgroundColor: '#007bff'
-          }]
+            pointBackgroundColor: '#2aff00'
+          }, {
+              label: 'Plant 1',
+            data: dataRows.map(row => row.soil_humid_1),
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#D500FF',
+            borderWidth: 4,
+            pointBackgroundColor: '#D500FF'
+          },{
+              label: 'Plant 2',
+            data: dataRows.map(row => row.soil_humid_2),
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#20DFD1',
+            borderWidth: 4,
+            pointBackgroundColor: '#20DFD1'
+          }, {
+              label: 'Plant 3',
+            data: dataRows.map(row => row.soil_humid_3),
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#D7DF20',
+            borderWidth: 4,
+            pointBackgroundColor: '#D7DF20'
+          }, ]
         },
         options: {
           scales: {
@@ -150,8 +176,6 @@
       
   }
 
-
-    
   // Load data and render chart on page load
   loadSensorData();
 })();
