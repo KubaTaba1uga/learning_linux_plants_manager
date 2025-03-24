5dc36d1e2b540faf7ed6630fa5a365c5b676b669
KubaTaba1uga <luna7337anul@proton.me>
2025-03-24 09:38:27 +0100

    move overlay into packages

--- buildroot/package/read_sensors_job/read_sensors_job.mk
+++ buildroot/package/read_sensors_job/read_sensors_job.mk
@@ -4,10 +4,29 @@ READ_SENSORS_JOB_SITE_METHOD = local
 READ_SENSORS_JOB_SITE = ../read_sensors_job
 READ_SENSORS_JOB_DEPENDENCIES = python3
 
+# Define separate variables for the cron directory and file.
+CRON_DIR = $(TARGET_DIR)/etc/cron/crontabs
+CRON_FILE = $(CRON_DIR)/root
+
 define READ_SENSORS_JOB_INSTALL_TARGET_CMDS
 	chmod 0755 $(@D)/read_sensors_job.py
 	cp $(@D)/read_sensors_job.py $(TARGET_DIR)/opt
+
+	# Ensure the cron directory exists
+	@if [ ! -d "$(CRON_DIR)" ]; then \
+	  mkdir -p "$(CRON_DIR)"; \
+	fi
+
+	# Ensure the cron file exists
+	@if [ ! -f "$(CRON_FILE)" ]; then \
+	  touch "$(CRON_FILE)"; \
+	fi
+
+	# Append the job line if missing
+	CRON_LINE='0 * * * * python3 /opt/read_sensors_job.py'
+	@if ! grep -qxF "$$CRON_LINE" "$(CRON_FILE)"; then \
+	  echo "$$CRON_LINE" >> "$(CRON_FILE)"; \
+	fi
 endef
 
 $(eval $(generic-package))
-
--- buildroot/package/read_sensors_web/read_sensors_web.mk
+++ buildroot/package/read_sensors_web/read_sensors_web.mk
@@ -6,9 +6,19 @@ READ_SENSORS_WEB_DEPENDENCIES = python3
 READ_SENSORS_WEB_DEPENDENCIES += python-fastapi
 READ_SENSORS_WEB_DEPENDENCIES += python-uvicorn
 
+# Define separate variables for the cron directory and file.
+INIT_DIR = $(TARGET_DIR)/etc/init.d/
+
 define READ_SENSORS_WEB_INSTALL_TARGET_CMDS
 	chmod 0755 $(@D)/read_sensors_web.py
 	cp -r $(@D)/read_sensors_web.py $(@D)/frontend $(TARGET_DIR)/opt
+
+	# Ensure the init directory exists
+	@if [ ! -d "$(INIT_DIR)" ]; then \
+	  mkdir -p "$(INIT_DIR)"; \
+	fi
+
+	cp $(@D)/S50read_sensors_web $(INIT_DIR)/
 endef
 
 $(eval $(generic-package))
