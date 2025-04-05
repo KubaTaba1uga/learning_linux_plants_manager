bdef7600002e2fa5b76afe75740c722cf15b64e5
KubaTaba1uga <luna7337anul@proton.me>
2025-04-05 14:33:30 +0200

    fix bugs

--- buildroot/package/read_sensors_job/read_sensors_job.mk
+++ buildroot/package/read_sensors_job/read_sensors_job.mk
@@ -4,26 +4,18 @@ READ_SENSORS_JOB_SITE_METHOD = local
 READ_SENSORS_JOB_SITE = ../read_sensors_job
 READ_SENSORS_JOB_DEPENDENCIES = python3
 
-CRON_DIR = $(TARGET_DIR)/etc/cron/crontabs
-CRON_FILE = $(CRON_DIR)/root
-CRON_LINE = 0 * * * * python3 /opt/read_sensors_job.py
+READ_SENSORS_JOB_CRON_DIR = $(TARGET_DIR)/etc/cron/crontabs
+READ_SENSORS_JOB_CRON_FILE = $(READ_SENSORS_JOB_CRON_DIR)/root
+READ_SENSORS_JOB_CRON_LINE = 0 * * * * python3 /opt/read_sensors_job.py
 
 define READ_SENSORS_JOB_INSTALL_TARGET_CMDS
+	echo ">>> Installing read_sensors_job <<<"
 	chmod 0755 $(@D)/read_sensors_job.py
 	cp $(@D)/read_sensors_job.py $(TARGET_DIR)/opt
 
-	# Ensure the cron directory exists
-	if [ ! -d "$(CRON_DIR)" ]; then \
-	  mkdir -p "$(CRON_DIR)"; \
-	fi
-
-	# Ensure the cron file exists
-	if [ ! -f "$(CRON_FILE)" ]; then \
-	  touch "$(CRON_FILE)"; \
-	fi
-
-	# Append the job line if missing
-	grep -qxF "$(CRON_LINE)" "$(CRON_FILE)" || echo "$(CRON_LINE)" >> "$(CRON_FILE)"
+	mkdir -p $(READ_SENSORS_JOB_CRON_DIR)
+	touch $(READ_SENSORS_JOB_CRON_FILE)
+	grep -qxF '$(READ_SENSORS_JOB_CRON_LINE)' $(READ_SENSORS_JOB_CRON_FILE) || echo '$(READ_SENSORS_JOB_CRON_LINE)' >> $(READ_SENSORS_JOB_CRON_FILE)
 endef
 
 $(eval $(generic-package))
